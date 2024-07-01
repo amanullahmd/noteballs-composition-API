4899e7fca05d3a4d06c07f9728d189137ed64c9f
Habibur Rahman Habib <rahman.habib@orwonet.de>
2024-07-01 11:57:29 +0200

    add updated

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,21 @@
-<script setup>
-import navbar from './components/layout/navbar.vue'
-</script>
-
 <template>
   <navbar />
   <div class="container is-max-desktop px-2 py-4">
     <RouterView />
   </div>
 </template>
+
+<script setup>
+import { onMounted } from 'vue';
+import navbar from './components/layout/navbar.vue'
+import { useStoreNotes } from './stores/storeNotes';
+
+const storeNotes = useStoreNotes()
+
+onMounted(()=> {
+  storeNotes.getNotes()
+})
+</script>
 
 <style>
 @import 'bulma/css/bulma.min.css';
--- src/stores/storeNotes.js
+++ src/stores/storeNotes.js
@@ -1,9 +1,12 @@
 import { defineStore } from 'pinia'
+import { collection, getDocs, onSnapshot } from 'firebase/firestore'
+import { db } from '@/js/firebase'
+
 export const useStoreNotes = defineStore('StoreNotes', {
   state: () => {
     return {
       notes: [
-        {
+        /* {
           id: 'id1',
           content:
             ' Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris.'
@@ -11,11 +14,39 @@ export const useStoreNotes = defineStore('StoreNotes', {
         {
           id: 'id2',
           content: ' Lorem ipsum dolor sit amet, consectetur woo!'
-        }
+        } */
       ]
     }
   },
   actions: {
+    async getNotes() {
+      /* const querySnapshot = await getDocs(collection(db, 'notes'))
+      querySnapshot.forEach((doc) => {
+
+        let note = {
+          id: doc.id,
+          content: doc.data().content
+        }
+        this.notes.push(note)
+        console.log('note => ', note)
+      }) */
+
+      onSnapshot(collection(db, 'notes'), (querySnapshot) => {
+        let notes = []
+        querySnapshot.forEach((doc) => {
+          let note = {
+            id: doc.id,
+            content: doc.data().content
+          }
+          notes.push(note)
+        });
+
+        this.notes = notes
+        
+      });
+
+
+    },
     addNote(newNotesContent) {
       let id = new Date().getTime().toString()
 
@@ -28,15 +59,15 @@ export const useStoreNotes = defineStore('StoreNotes', {
     deleteNote(deleteNoteId) {
       this.notes = this.notes.filter((note) => note.id !== deleteNoteId)
     },
-    updateNote(id, content){
-      const index = this.notes.findIndex(note => note.id === id)
+    updateNote(id, content) {
+      const index = this.notes.findIndex((note) => note.id === id)
       this.notes[index].content = content
     }
   },
   getters: {
     getNoteContent: (state) => {
       return (noteId) => {
-        const note = state.notes.find(note => note.id === noteId)
+        const note = state.notes.find((note) => note.id === noteId)
         return note ? note.content : null
       }
     },
@@ -45,7 +76,7 @@ export const useStoreNotes = defineStore('StoreNotes', {
     },
     totalCharacters: (state) => {
       let totalCharacters = 0
-      state.notes.forEach(note => totalCharacters += note.content.length)
+      state.notes.forEach((note) => (totalCharacters += note.content.length))
       return totalCharacters
     }
   }
