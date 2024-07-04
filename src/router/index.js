156bc598619b26d59f2de78d189366f7a6da53a6
Habibur Rahman Habib <rahman.habib@orwonet.de>
2024-07-04 10:32:17 +0200

    add navigation guird

--- src/router/index.js
+++ src/router/index.js
@@ -1,20 +1,77 @@
 import { createRouter, createWebHistory } from 'vue-router'
-
+import { useStoreAuth } from '@/stores/storeAuth'
 import ViewNotes from '@/view/ViewNotes.vue'
 import ViewStats from '@/view/ViewStats.vue'
 import ViewEditNote from '@/view/ViewEditNote.vue'
 import ViewAuth from '@/view/ViewAuth.vue'
 
+const appTitle = 'Noteballs'
+const DEFAULT_TITLE = 'Noteballs'
+
 const routes = [
-  { path: '/', name: 'home', component: ViewNotes },
-  { path: '/editNote/:id', name: 'editNote', component: ViewEditNote },
-  { path: '/stats', name: 'stats', component: ViewStats },
-  { path: '/auth', name: 'auth', component: ViewAuth }
+  {
+    path: '/',
+    name: 'home',
+    component: ViewNotes,
+    meta: {
+      title: `${appTitle} - Notes`
+    }
+  },
+  {
+    path: '/editNote/:id',
+    name: 'editNote',
+    component: ViewEditNote,
+    meta: {
+      title: `${appTitle} - Update note`
+    }
+  },
+  {
+    path: '/stats',
+    name: 'stats',
+    component: ViewStats,
+    meta: {
+      title: `${appTitle} - Stats`
+    }
+  },
+  {
+    path: '/auth',
+    name: 'auth',
+    component: ViewAuth,
+    meta: {
+      title: `${appTitle} - Register`
+    }
+  }
 ]
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes
+})
+
+// Navigation guard
+router.beforeEach(async (to, from, next) => {
+  document.title = to.meta?.title ?? DEFAULT_TITLE
+
+  const storeAuth = useStoreAuth()
+
+  const authorizedRoutes = ['home', 'editNote', 'stats']
+  const publicRoutes = ['auth']
+
+  if (authorizedRoutes.includes(to.name)) {
+    if (!storeAuth.user.id) {
+      // If not logged in, redirect to home
+      next({ name: 'auth' })
+    } else {
+      // If logged in, allow navigation
+      next()
+    }
+  } else if (publicRoutes.includes(to.name)) {
+    // Allow access to public routes
+    next()
+  } else {
+    // If route not found, redirect to home
+    next({ name: 'auth' })
+  }
 })
 
 export default router
